e1535ac9735b078af6983870dc0fdf896fdb71cc
Marek Wojtaszek <kontakt@marekwojtaszek.com>
2020-01-28 13:51:02 +1100

    Gallery v2

--- gallery.js
+++ gallery.js
@@ -1,176 +1,197 @@
 (() => {
-  'use strict'
+  "use strict";
 
-  const body = document.body
+  const body = document.body;
+  const aE = document.addEventListener;
+  const rE = document.removeEventListener;
+
   const cssStyles = {
-    container: {
-      'height': '100%',
-      'width': '100%',
-      'position': 'absolute',
-      'top': '0',
-      'left': '0',
-      'color': '#fff',
-      'background': '#000',
-      'text-align': 'center',
-      'z-index': '100000',
-      'overflow': 'hidden'
+    html: {
+      height: "100%"
     },
-    status: {
-      'width': '80px',
-      'position': 'absolute',
-      'top': '0',
-      'right': '0',
-      'padding': '6px 0',
-      'background': '#000',
-      'text-align': 'center',
-      'z-index': '1000000'
+    body: {
+      height: "100%",
+      padding: 0
     },
-    slide: {
-      'height': '100%',
-      'width': '100%',
-      'display': 'none',
-      'background-position': 'center center',
-      'background-repeat': 'no-repeat',
-      'background-size': 'contain'
+    ".fsContainer": {
+      height: "100%",
+      width: "100%",
+      position: "absolute",
+      top: "0",
+      left: "0",
+      background: "#000",
+      overflow: "hidden",
+      "z-index": "100000",
+      "-webkit-user-select": "none"
     },
-    autoplay: {
-      'color': '#00c0ff'
+    ".fsStatus": {
+      width: "80px",
+      position: "absolute",
+      top: "0",
+      right: "0",
+      padding: "6px 0",
+      color: "#fff",
+      background: "#000",
+      "text-align": "center",
+      "z-index": "1000000"
+    },
+    ".fsSlide": {
+      height: "100%",
+      width: "100%",
+      "background-position": "center center",
+      "background-repeat": "no-repeat",
+      "background-size": "contain"
+    },
+    ".fsAutoplay": {
+      "border-bottom": "1px solid #00c0ff"
     }
-  }
+  };
 
-  const closest = (el) => el && (el.id.length > 0 ? el : closest(el.parentNode))
+  const closest = el => el && (el.id.length > 0 ? el : closest(el.parentNode));
 
-  const addCssStyles = (cssStyles) => {
-    const style = document.createElement('style')
-    style.appendChild(document.createTextNode(''))    // WebKit hack
-    document.head.appendChild(style)
+  const getEl = (type, className, initContent) => {
+    const el = document.createElement(type);
+    if (className) el.classList.add(className);
 
-    Object.keys(cssStyles).forEach((el) => {
-      style.sheet.insertRule(`.fs-gallery-${el}
+    if (Array.isArray(initContent)) {
+      initContent.forEach(content => el.appendChild(content));
+    } else if (typeof initContent === "object") {
+      el.appendChild(initContent);
+    } else if (typeof initContent === "string") {
+      el.innerText = initContent;
+    }
+    return el;
+  };
+
+  const addCssStyles = cssStyles => {
+    const style = getEl("style", null, document.createTextNode(""));
+    document.head.appendChild(style);
+
+    Object.keys(cssStyles).forEach(el =>
+      style.sheet.insertRule(
+        `${el}
         ${JSON.stringify(cssStyles[el])
-              .replace(/\"/gi,'')
-              .replace(/\,/gi,';')}`, 0)
-    })
-  }
+          .replace(/\"/gi, "")
+          .replace(/\,/gi, ";")}`,
+        0
+      )
+    );
+  };
 
-  const renderDiv = (className, container) => {
-    const el = document.createElement('div')
-    el.classList.add(`fs-gallery-${className}`)
-
-    if (container) container.appendChild(el)
-
-    return el
-  }
-
-  const renderSlides = (className, container, images) => {
-    const slides = []
-    let slide
-
-    images.forEach((image, i) => {
-      image.style.opacity = 0
-      slide = renderDiv(className, container)
-      slide.style.backgroundImage = 'url(' + image.src + ')',
-      slide.appendChild(image)
-      slides[i] = slide
-    })
-
-    slides[0].style.display = 'block'   // show first image
-
-    return slides
-  }
-
-  const keyActions = (gallery) => ({which}) => {
+  const keyBinding = gallery => ({ which }) => {
     const keys = {
-      '0'   : gallery.showNextSlide,  // spacebar
-      '32'  : gallery.showNextSlide,  // spacebar
-      '112' : gallery.autoplay,       // 'p' key
-      '108' : gallery.showNextSlide,  // 'l' key
-      '107' : gallery.showPrevSlide   // 'k' key
+      "0": gallery.showNextSlide, // spacebar
+      "32": gallery.showNextSlide, // spacebar
+      "112": gallery.autoplay, // 'p' key
+      "108": gallery.showNextSlide, // 'l' key
+      "107": gallery.showPrevSlide // 'k' key
+    };
+
+    if (typeof keys[which] !== "function") return;
+    else keys[which]();
+  };
+
+  class Status {
+    constructor(initText) {
+      this.content = getEl("div", "fsStatus", initText);
     }
 
-    if (typeof keys[which] !== 'function') return
-    else keys[which]()
+    get text() {
+      return this.content;
+    }
+
+    set text(text) {
+      this.content.innerText = text;
+    }
+
+    set autoplay(value) {
+      this.content.classList.toggle("fsAutoplay", value);
+    }
   }
 
   class Gallery {
     constructor(images) {
-      this._container = renderDiv('container')
-      this._status    = renderDiv('status', this._container)
-      this._slides    = renderSlides('slide', this._container, images)
-      this._index     = 0
-      this._interval  = null
-      this._updateStatus(this._index)   // set initial status
+      this.index = 0;
+      this.interval = null;
+      this.slides = this.getSlides(images);
+      this.status = new Status(`${this.index + 1} / ${this.slides.length}`);
+      this.container = getEl("div", "fsContainer", [
+        this.status.text,
+        ...this.slides
+      ]);
 
-      this.showNextSlide = this._showNextSlide.bind(this)
-      this.showPrevSlide = this._showPrevSlide.bind(this)
-      this.autoplay      = this._autoplay.bind(this)
+      this.showNextSlide = this.changeSlide.bind(this, 1);
+      this.showPrevSlide = this.changeSlide.bind(this, 0);
+      this.autoplay = this.autoplay.bind(this);
     }
 
     get html() {
-      return this._container
+      return this.container;
     }
 
-    _updateStatus(index) {
-      this._status.innerText = parseInt(index + 1, 10) + ' / ' + this._slides.length
+    getSlides(images) {
+      return Array.from(images).reduce((slides, image) => {
+        const slide = getEl("div", "fsSlide", image);
+        slide.style.backgroundImage = `url(${image.src})`;
+        image.style.opacity = 0;
+
+        return [...slides, slide];
+      }, []);
     }
 
-    _showNextSlide() {
-      if (this._index < this._slides.length - 1) {
-        this._slides[this._index].style.display   = 'none'
-        this._slides[++this._index].style.display = 'block'
-        this._updateStatus(this._index)
+    changeSlide(direction) {
+      const nextIndex = direction ? this.index + 1 : this.index - 1;
+
+      if (0 <= nextIndex && nextIndex <= this.slides.length - 1) {
+        Object.assign(this.slides[this.index].style, { height: 0, width: 0 });
+        Object.assign(this.slides[nextIndex].style, {
+          height: "100%",
+          width: "100%"
+        });
+        this.index = nextIndex;
+        this.status.text = `${nextIndex + 1} / ${this.slides.length}`;
       }
     }
 
-    _showPrevSlide() {
-      if (this._index > 0) {
-        this._slides[this._index].style.display   = 'none'
-        this._slides[--this._index].style.display = 'block'
-        this._updateStatus(this._index)
-      }
-    }
-
-    _autoplay() {
-      if(this._interval) {
-        this._status.classList.remove('fs-gallery-autoplay')
-        this._interval = window.clearInterval(this._interval)
+    autoplay() {
+      if (this.interval) {
+        this.status.autoplay = 0;
+        this.interval = window.clearInterval(this.interval);
       } else {
-        this._status.classList.add('fs-gallery-autoplay')
-        this._interval = setInterval(this.showNextSlide, 4000)
+        this.status.autoplay = 1;
+        this.interval = setInterval(this.showNextSlide, 4000);
       }
     }
   }
 
-  function generateGallery (e) {
-    if (e.target.tagName === 'IMG' && e.target !== e.currentTarget) {
-      const images = document.querySelectorAll(`#${closest(e.target.parentNode).id} img`)
-      const gallery = new Gallery(images)
+  function generateGallery(e) {
+    if (e.target.tagName === "IMG" && e.target !== e.currentTarget) {
+      const images = document.querySelectorAll(
+        `#${closest(e.target.parentNode).id} img`
+      );
+      const gallery = new Gallery(images);
 
-      document.documentElement.style.height = '100%'
-      Object.assign(body.style, { height: '100%', padding: 0 })
-      body.innerHTML = ''     // reset page content
-      body.appendChild(gallery.html)
+      body.innerHTML = ""; // reset page content
+      body.appendChild(gallery.html);
 
-      addCssStyles(cssStyles)
+      addCssStyles(cssStyles);
 
-      document.removeEventListener('click', generateGallery)
-
-      document.addEventListener('click', gallery.showNextSlide, false)
-      document.addEventListener('keypress', keyActions(gallery), false)
+      rE("click", generateGallery);
+      aE("click", gallery.showNextSlide, false);
+      aE("keypress", keyBinding(gallery), false);
     }
 
-    e.preventDefault()
-    e.stopPropagation()
+    e.preventDefault();
+    e.stopPropagation();
   }
 
-  function activateClickListener () {
+  function activateClickListener() {
     if (!document.processed) {
-      document.processed = true      // allow gallery to be processed just once
-      document.addEventListener('click', generateGallery, false)
+      document.processed = true; // allow gallery to be processed just once
+      aE("click", generateGallery, false);
     }
   }
 
-  document.processed = document.processed || false
-
-  activateClickListener()
-})()
+  document.processed = document.processed || false;
+  activateClickListener();
+})();
